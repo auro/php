f415ba864fd64ff8450ff8cfe6da5f136afc3626
Auro Florentino <auro@merco.net>
2021-02-22 21:17:17 -0300

    Entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
-/usr/bin/supervisord -c /etc/supervisor/conf.d/supervisor.conf
+if [ $# -eq 0 ]
+  then
+    echo "No arguments supplied"
+    /usr/bin/supervisord -c /etc/supervisor/conf.d/supervisor.conf
+else
+    exec "$@"
+fi
